3cb8fb4000c0a0f32b86772db01c941033ad2f36
UtopiaBeam <utopiabeam@gmail.com>
2020-06-14 22:39:04 +0700

    ➕ Add vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,3 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div>This is homepage</div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
