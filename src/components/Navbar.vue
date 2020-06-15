58d8611cf5195943fdff827ef87f79a58aa9cdb3
UtopiaBeam <utopiabeam@gmail.com>
2020-06-15 22:11:47 +0700

    📱 Update responsive behavior

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <v-app-bar color="primary" dark dense clipped-left app>
+      <v-app-bar-nav-icon v-if="!isLargeScreen" @click="toggleDrawer" />
       <v-toolbar-title>SCRUMBLE</v-toolbar-title>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" expand-on-hover mini-variant clipped app>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      :mini-variant="isLargeScreen"
+      :expand-on-hover="isLargeScreen"
+      app
+    >
       <v-list nav dense>
+        <v-list-item v-if="!isLargeScreen" @click="toggleDrawer">
+          <v-list-item-icon>
+            <v-icon>mdi-page-first</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content />
+        </v-list-item>
+        <v-divider />
         <v-list-item v-for="menu in menus" :key="menu.title" :to="menu.href">
           <v-list-item-icon>
             <v-icon>{{ menu.icon }}</v-icon>
@@ -25,7 +39,7 @@ import { Vue, Component } from 'vue-property-decorator'
 
 @Component
 export default class Navbar extends Vue {
-  private drawer = true
+  private drawer = this.isLargeScreen
   private readonly menus = [
     { title: 'Backlog', icon: 'mdi-view-list', href: '/app/backlog' },
     { title: 'Kanban Board', icon: 'mdi-view-column', href: '/app/board' },
@@ -33,5 +47,13 @@ export default class Navbar extends Vue {
     { title: 'Planning Poker', icon: 'mdi-cards', href: '/app/poker' },
     { title: 'Retrospective', icon: 'mdi-replay', href: '/app/retrospective' },
   ]
+
+  get isLargeScreen(): boolean {
+    return this.$vuetify.breakpoint.lgAndUp
+  }
+
+  private toggleDrawer() {
+    this.drawer = !this.drawer
+  }
 }
 </script>
