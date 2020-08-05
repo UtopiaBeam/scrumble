3ade708318b5cfb29ce96a4943664a6a9a838b11
UtopiaBeam <utopiabeam@gmail.com>
2020-08-05 12:09:31 +0700

    🔥 Remove import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 import AppHome from '../views/app/Home.vue'
 import { appRoutes } from './app'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
+const routes: RouteConfig[] = [
   {
     path: '/login',
     name: 'Login',
