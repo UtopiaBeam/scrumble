235d96daf5c54e26a911f887d571bf4b1b9b3e34
UtopiaBeam <utopiabeam@gmail.com>
2020-08-29 22:14:27 +0700

    ✨ Add hints and rules

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,22 +5,62 @@
         <v-card width="450" elevation="4">
           <v-card-title class="text-h4 justify-center">SCRUMBLE</v-card-title>
           <v-card-text>
-            <p class="my-1 font-weight-medium">Username</p>
-            <v-text-field v-model="user.username" outlined />
-            <p class="my-1 font-weight-medium">Email</p>
-            <v-text-field v-model="user.email" outlined />
-            <p class="my-1 font-weight-medium">Password</p>
-            <v-text-field v-model="user.password" outlined type="password" />
-            <p class="my-1 font-weight-medium">Confirm password</p>
-            <v-text-field v-model="confirmPassword" outlined type="password" />
-            <v-row justify="space-between" align="center" no-gutters>
-              <v-col cols="auto">
-                Already have an account? <a href="/login">Login</a>
-              </v-col>
-              <v-col cols="auto">
-                <v-btn color="primary">Register</v-btn>
-              </v-col>
-            </v-row>
+            <v-form ref="form" v-model="formValid" @submit.prevent="register">
+              <v-text-field
+                v-model="user.username"
+                label="Username"
+                :rules="rules.username"
+                :error="error"
+                outlined
+              />
+              <v-text-field
+                v-model="user.email"
+                label="Email"
+                :rules="rules.email"
+                :error="error"
+                outlined
+              />
+              <v-text-field
+                v-model="user.password"
+                label="Password"
+                :rules="rules.password"
+                :error="error"
+                outlined
+                type="password"
+                persistent-hint
+              >
+                <template v-slot:append>
+                  <v-tooltip right>
+                    <template v-slot:activator="{ on }">
+                      <v-icon v-on="on" color="grey">mdi-alert-circle</v-icon>
+                    </template>
+                    Password must consists of
+                    <ul>
+                      <li>8 characters or more</li>
+                      <li>One lowercase character</li>
+                      <li>One uppercase character</li>
+                      <li>One numeric character</li>
+                    </ul>
+                  </v-tooltip>
+                </template>
+              </v-text-field>
+              <v-text-field
+                v-model="confirmPassword"
+                label="Confirm password"
+                :error="error"
+                :rules="rules.confirmPassword"
+                outlined
+                type="password"
+              />
+              <v-row justify="space-between" align="center" no-gutters>
+                <v-col cols="auto">
+                  Already have an account? <a href="/login">Login</a>
+                </v-col>
+                <v-col cols="auto">
+                  <v-btn type="submit" color="primary">Register</v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
           </v-card-text>
         </v-card>
       </v-row>
@@ -30,15 +70,50 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { vxm } from '@/store'
 
 @Component
-export default class Login extends Vue {
+export default class Register extends Vue {
   private user = {
     username: '',
     email: '',
     password: '',
   }
+  private error = false
+  private formValid = true
   private confirmPassword = ''
+  private readonly rules = {
+    username: [(v: string) => v.trim().length > 0 || 'Username is required.'],
+    email: [
+      (v: string) => !!v || 'Email is required.',
+      (v: string) => /.+@.+\..+/.test(v) || 'Email must be valid.',
+    ],
+    password: [
+      (v: string) =>
+        v.length >= 8 || 'Password must be at least 8 characters long.',
+      (v: string) =>
+        /[a-z]/g.test(v) ||
+        'Password must consists of one lowercase character.',
+      (v: string) =>
+        /[A-Z]/g.test(v) ||
+        'Password must consists of one uppercase character.',
+      (v: string) =>
+        /[\d]/g.test(v) || 'Password must consists of one numeric character.',
+    ],
+    confirmPassword: [
+      (v: string) => v === this.user.password || 'Password mismatched',
+    ],
+  }
+
+  async register() {
+    try {
+      this.error = false
+      await vxm.auth.register(this.user)
+    } catch (e) {
+      console.error(e)
+      this.error = true
+    }
+  }
 }
 </script>
 
